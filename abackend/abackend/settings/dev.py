da1caa58e6af0233b165f170bd51ee4c67f22637
hotwag <mhotwagner@gmail.com>
2015-11-17 00:18:16 -0500

    Setup project and initial app

---
+++ abackend/abackend/settings/dev.py
@@ -0,0 +1,12 @@
+from setings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'wordsdev',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
